99a2062aa1695d5ab5769b6b67e1dd3b92c35954
Влад Зиненко <deadbeat686@gmail.com>
2024-12-17 01:27:06 +0300

    Добавьте файлы проекта.

---
+++ Practice.Tests/CalculatorTest.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Practice;
+
+namespace Practice.Tests
+{
+    [TestFixture]
+    public class CalculatorTest
+    {
+
+        [Test]
+        public void AdditionalMustReturnCorrectValue()
+        {
+            Calculator calculator = new Calculator();
+            int result = calculator.Additional(10, 10);
+            Assert.That(result == 20);
+        }
+
+        [Test]
+        public void SubtractionlMustReturnCorrectValue()
+        {
+            Calculator calculator = new Calculator();
+            int result = calculator.Subtraction(20, 10);
+            Assert.That(result == 10);
+        }
+
+        [Test]
+        public void MultiplicationMustReturnCorrectValue()
+        {
+            Calculator calculator = new Calculator();
+            int result = calculator.Multiplication(10, 10);
+            Assert.That(result == 100);
+        }
+
+        [Test]
+        public void DivisionMustReturnCorrectValue()
+        {
+            Calculator calculator = new Calculator();
+            int result = calculator.Division(10, 10);
+            Assert.That(result == 1);
+        }
+
+        [Test]
+        public void DivisionMustThrowExceptionOnZeroDivision()
+        {
+            Calculator calculator = new Calculator();
+            Assert.Throws<ArgumentException>(() => calculator.Division(10, 0));
+        }
+
+    }
+}
---
+++ Practice/Calculator.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Practice
+{
+    public class Calculator
+    {
+       
+
+
+        public int Additional(int a, int b)
+        {
+            return a + b;
+        }
+
+        public int Subtraction(int a, int b)
+        {
+            return a - b;
+        }
+
+        public int Multiplication(int a, int b)
+        {
+            return a * b;
+        }
+
+        public int Division(int a, int b)
+        {
+            if (b == 0)
+                throw new ArgumentException("Деление на ноль невозможно.");
+            return a / b;
+        }
+
+    }
+}
